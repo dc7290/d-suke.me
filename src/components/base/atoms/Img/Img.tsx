bcec77e2a42d175c77b0c1f5917857a90a8fe6e1
dc7290 <dhkh.cba0927@gmail.com>
2022-03-04 04:01:07 +0900

    feat: add components `Img`

---
+++ src/components/base/atoms/Img/Img.tsx
@@ -0,0 +1,12 @@
+import { DetailedHTMLProps, ImgHTMLAttributes } from 'react'
+
+type Props = DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> & {
+  alt: string
+}
+
+const Img = (props: Props) => {
+  // eslint-disable-next-line jsx-a11y/alt-text
+  return <img {...props} decoding="async" />
+}
+
+export default Img
