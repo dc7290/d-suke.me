2a20415304cc105f9dcb66ee7c57d8529330f8c1
dc7290 <dhkh.cba0927@gmail.com>
2022-03-06 00:46:55 +0900

    リポジトリ整理

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,13 +24,13 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
 const IndexPage: NextLayoutPage<Props> = ({ zennColumns }) => {
   return (
     <div className="container mx-auto px-4 pb-16 sm:px-6 lg:px-8 lg:pb-24">
-      <div className="mt-16 sm:mt-24">
+      <section className="mt-16 sm:mt-24">
         <h2 className="flex items-center space-x-4 text-2xl">Zenn</h2>
         <ZennAnchorList list={zennColumns} className="mt-5" />
         <ExternalAnchor className="mt-4" href="https://zenn.dev/d_suke">
           もっと見る
         </ExternalAnchor>
-      </div>
+      </section>
     </div>
   )
 }
--- src/utils/animation/fade.ts
+++
@@ -1,8 +0,0 @@
-import { Variants } from 'framer-motion'
-
-const fadeVariants: Variants = {
-  hidden: { opacity: 0 },
-  show: { opacity: 1 },
-}
-
-export default fadeVariants
